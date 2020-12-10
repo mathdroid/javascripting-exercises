c1feea54ce9a6537f805d73d568b557201e1cdcc
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2020-12-10 16:06:25 +0700

    passing functions

--- callbacks.js
+++ callbacks.js
@@ -1,17 +1,31 @@
 function questionName(name) {
-    return "Is your name " + name + "?";
+  return "Is your name " + name + "?";
+}
+
+function questionAge(age) {
+  return "Is your age " + age + "?";
+}
+
+function ngobrol(pertanyaan, asumsi, jawaban) {
+  const stringPertanyaan = pertanyaan(asumsi); // questionAge(17)
+  console.log(stringPertanyaan);
+  console.log(jawaban);
+}
+
+function getKotaByKoordinat(coord) {
+  if (coord[0] === 53 && coord[1] === 14) {
+    return "Bandung";
+  } else {
+    return "Jakarta";
   }
-  
-  function questionAge(age) {
-    return "Is your age " + age + "?";
-  }
-  
-  function ngobrol(pertanyaan, asumsi, jawaban) {
-    const stringPertanyaan = pertanyaan(asumsi); // questionAge(17)
-    console.log(stringPertanyaan);
-    console.log(jawaban);
-  }
-  
-  ngobrol(questionName, "Odi", "Ya, saya Odi");
-  ngobrol(questionAge, 17, "Tidak, saya tidak berusia 17 tahun");
-  
+}
+
+ngobrol(questionName, "Odi", "Ya, saya Odi");
+ngobrol(questionAge, 17, "Tidak, saya tidak berusia 17 tahun");
+ngobrol(
+  function (arg) {
+    return "Apakah anda tinggal di kota " + getKotaByKoordinat(arg) + "?";
+  },
+  [53, 17],
+  "Tidak, saya tidak tinggal di Bandung"
+);
